a82655c14c965bdf8f2a4d7ccd8c7ea9bff664ca
totononococo <tonoco@totononococonoMacBook-Pro.local>
2023-01-22 14:16:31 +0900

    Fix(card exercise

--- src/components/CardExercise.tsx
+++ src/components/CardExercise.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import Box from "@mui/material/Box"
-import Card from "@mui/material/Card"
-import Stack from "@mui/material/Stack"
-import Typography from '@mui/material/Typography'
+import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 const CardExercise = () => {
   const cardStyle = {
-    minWidth: '200px',
-    height: '200px',
+    minWidth: "200px",
+    height: "200px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   };
   return (
     <>
-      <Box sx={{ margin: '18px' }}>CardExercise</Box>
+      <Box sx={{ margin: "18px" }}>CardExercise</Box>
       <Box>
-        <Stack sx={{ display: 'flex', flexDirection: 'column', gap: '40px'}}>
+        <Stack sx={{ display: "flex", flexDirection: "column", gap: "40px" }}>
           <Card sx={cardStyle}>
-            <Typography>Toto is sleeping</Typography>
+            <Box sx={{position:"relative", }}>
+              <Typography sx={{}}>last</Typography>
+              <Typography>Next</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}>
+              <Typography>Toto is sleeping</Typography>
+            </Box>
           </Card>
           <Card sx={cardStyle}>
             <Typography>Toto is sleeping</Typography>
@@ -33,6 +47,6 @@ const CardExercise = () => {
       </Box>
     </>
   );
-}
+};
 
-export default CardExercise
+export default CardExercise;
